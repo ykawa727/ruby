6555b0bc7f204382853fd5ffd997540d112a2e91
EC2 Default User <ec2-user@ip-172-31-0-6.ec2.internal>
2021-04-29 11:53:09 +0000

    [Learn] 8

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,18 +1,17 @@
 def fizz_buzz(number)
-  if (number%3 == 0) && (number%5 == 0)   #if number % 15 == 0
-     "FizzBuzz"
-  elsif number%3 == 0
-     "fizz"
-  elsif number%5 == 0
-     "buzz"
+
+  if number % 15 == 0
+    puts "FizzBuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
   else
-     number.to_s
+    puts "#{number}"
   end
 end
 
-puts "数字を入力してください。"
-
-number = gets.to_i
-
-puts "結果は"
-puts fizz_buzz(number)
+puts "数字を入力してください"
+input = gets.to_i
+puts "結果は..."
+puts fizz_buzz(input)
--- greeting.rb
+++ greeting.rb
@@ -1,7 +1,6 @@
 def greeting(name)
-  return"Hello,#{name}!"
-  
-  "Good morning,#{name}!"
+  return "Hello, #{name}!"
+  "Good morning, #{name}"
 end
 
-puts  greeting('John')
+puts greeting('john')
